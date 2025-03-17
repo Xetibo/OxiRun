9c8f1bd134af3c7e3f8070c171625f710a7333c3
DashieTM <fabio.lenherr@gmail.com>
2025-03-17 21:47:32 +0100

    initial bar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,140 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::Display;
+
+use iced::keyboard::key::Named;
+use iced::widget::container::Style;
+use iced::widget::{column, container, row, scrollable, Column, Row};
+use iced::{event, futures, Alignment, Color, Element, Length, Renderer, Task, Theme};
+use indexmap::IndexMap;
+use oxiced::theme::get_theme;
+use oxiced::widgets::common::darken_color;
+use oxiced::widgets::oxi_button::{button, ButtonVariant};
+use oxiced::widgets::oxi_picklist::pick_list;
+use oxiced::widgets::oxi_text_input::text_input;
+
+use iced_layershell::actions::LayershellCustomActions;
+use iced_layershell::reexport::{Anchor, KeyboardInteractivity, Layer};
+use iced_layershell::settings::{LayerShellSettings, Settings};
+use iced_layershell::Application;
+
+pub fn main() -> Result<(), iced_layershell::Error> {
+    let settings = Settings {
+        layer_settings: LayerShellSettings {
+            size: Some((600, 600)),
+            exclusive_zone: 0,
+            anchor: Anchor::Left | Anchor::Right,
+            layer: Layer::Overlay,
+            margin: (100, 100, 100, 100),
+            keyboard_interactivity: KeyboardInteractivity::Exclusive,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
+    OxiRun::run(settings)
+}
+
+struct OxiRun {
+    theme: Theme,
+    filter_text: String,
+}
+
+impl Default for OxiRun {
+    fn default() -> Self {
+        Self {
+            theme: get_theme(),
+            filter_text: "".into(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    SetFilterText(String),
+}
+
+impl TryInto<LayershellCustomActions> for Message {
+    type Error = Self;
+    fn try_into(self) -> Result<LayershellCustomActions, Self::Error> {
+        Err(self)
+    }
+}
+
+fn box_style(theme: &Theme) -> Style {
+    let palette = theme.extended_palette();
+    Style {
+        background: Some(iced::Background::Color(darken_color(
+            palette.background.base.color,
+        ))),
+        border: iced::border::rounded(10),
+        ..container::rounded_box(theme)
+    }
+}
+
+fn wrap_in_rounded_box<'a>(
+    content: impl Into<Element<'a, Message, Theme, Renderer>>,
+) -> Element<'a, Message> {
+    container(content)
+        .style(box_style)
+        .align_x(Alignment::Center)
+        .padding(50)
+        .max_width(550)
+        .into()
+}
+
+impl Application for OxiRun {
+    type Message = Message;
+    type Flags = ();
+    type Theme = Theme;
+    type Executor = iced::executor::Default;
+
+    fn new(_flags: ()) -> (Self, Task<Message>) {
+        (
+            Self {
+                ..Default::default()
+            },
+            iced::widget::text_input::focus("search_box"),
+        )
+    }
+
+    fn namespace(&self) -> String {
+        String::from("OxiRun")
+    }
+
+    fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::SetFilterText(value) => {
+                self.filter_text = value;
+                Task::none()
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        wrap_in_rounded_box(
+            Column::new().push(
+                text_input(
+                    "Enter text to find",
+                    self.filter_text.as_str(),
+                    Message::SetFilterText,
+                )
+                .id("search_box"),
+            ),
+        )
+    }
+
+    fn theme(&self) -> Theme {
+        self.theme.clone()
+    }
+
+    // remove the annoying background color
+    fn style(&self, theme: &Self::Theme) -> iced_layershell::Appearance {
+        let palette = theme.extended_palette();
+        iced_layershell::Appearance {
+            background_color: iced::Color::TRANSPARENT,
+            text_color: palette.background.base.text,
+        }
+    }
+
+    fn scale_factor(&self) -> f64 {
+        1.0
+    }
 }
